a5812b6daf625e646eaddc213effe3bfa8deb180
Aditya Deelip Toke <atoke@ischoolconnect.com>
2021-04-05 01:52:22 +0530

    updated the package.json file and base setup for html,js and angular js

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,37 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+import * as path from 'path'
+import * as fs from 'fs'
+let currentPanel: vscode.WebviewPanel | undefined = undefined;
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "git-branches-ci-cd" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('git-branches-ci-cd.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
+	let disposable = vscode.commands.registerCommand('git-branches-ci-cd.initBranchTask', () => {
 		vscode.window.showInformationMessage('Hello World from Git Branches CI/CD!');
+
+
+		const columnToShowIn = vscode.window.activeTextEditor
+			? vscode.window.activeTextEditor.viewColumn
+			: undefined;
+
+		if (currentPanel) {
+			currentPanel.reveal(columnToShowIn);
+		} else {
+			currentPanel = vscode.window.createWebviewPanel(
+				'parentWebViewScreen',
+				'Git: Branch CI/CD',
+				vscode.ViewColumn.One,
+				{
+					enableScripts: true
+				}
+			);
+			currentPanel.webview.html = getWebviewContent(context);
+		}
 	});
 
 	context.subscriptions.push(disposable);
 }
 
+function getWebviewContent(context: vscode.ExtensionContext): string {
+	const filePath: vscode.Uri = vscode.Uri.file(path.join(context.extensionPath, 'src', 'website', 'index.html'));
+	return fs.readFileSync(filePath.fsPath, 'utf8');
+}
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
